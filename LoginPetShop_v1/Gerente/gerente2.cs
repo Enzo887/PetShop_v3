5025dde8adff298f81d636638756c6749d125762
Enzo887 <enzoduarte2014@gmail.com>
2025-05-21 22:53:10 -0300

    prueba de .gitatributte

---
+++ LoginPetShop_v1/Gerente/gerente2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginPetShop_v1.Gerente
+{
+    public partial class gerente2 : UserControl
+    {
+        public gerente2()
+        {
+            InitializeComponent();
+        }
+    }
+}
