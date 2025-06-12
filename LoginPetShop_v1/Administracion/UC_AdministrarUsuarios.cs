0a18badce6d2b37fb6ae53bf09ceead1b8ddf5bc
unknown <villordomarco2020@gmail.com>
2025-06-12 20:25:32 -0300

    Desarrolle el CU RegistrarUsuario

--- LoginPetShop_v1/Administracion/UC_AdministrarUsuarios.cs
+++ LoginPetShop_v1/Administracion/UC_AdministrarUsuarios.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BE;
 
 namespace LoginPetShop_v1.Administracion
 {
@@ -15,6 +16,102 @@ namespace LoginPetShop_v1.Administracion
         public UC_AdministrarUsuarios()
         {
             InitializeComponent();
+        }
+
+        private void btnCrearUsuario_Click(object sender, EventArgs e)
+        {
+            BLL.Administrador unAdmin = new BLL.Administrador();
+            string tipoUsuario = comboxSeleccionRolNuevoUsuario.SelectedItem.ToString();
+            //validaciones
+            string nombre = tboxNombreUsuarioCrear.Text;
+            string apellido = tboxApellidoUsuarioCrear.Text;
+            string nombreUsuario = tboxNUsuarioCrear.Text;
+            int dni = int.Parse(tboxDniUsuario.Text);
+
+            // Validaciones generales
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                MessageBox.Show("Por favor, ingrese un nombre para el usuario.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(apellido))
+            {
+                MessageBox.Show("Por favor, ingrese un apellido para el usuario.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(nombreUsuario))
+            {
+                MessageBox.Show("Por favor, ingrese un nombre de usuario.");
+                return;
+            }
+            if (nombreUsuario.Length < 8)
+            {
+                MessageBox.Show("El nombre de usuario debe tener al menos 8 caracteres.");
+                return;
+            }
+
+            BE.Usuario unUsuario;
+
+            switch (tipoUsuario)
+            {
+                case "Administrador":
+                    unUsuario = new BE.Administrador();
+                    {
+                        unUsuario.Nombre = nombre;
+                        unUsuario.Apellido = apellido;
+                        unUsuario.NombreUsuario = nombreUsuario;
+                        unUsuario.Contraseña = tboxContraseñaUsuarioCrear.Text;
+                        unUsuario.DNI = dni;
+                        //unUsuario.Rol = comboxSeleccionRolNuevoUsuario.SelectedItem.ToString();
+                        unUsuario.Rol = "1";
+                        unAdmin.RegistrarUsuario(unUsuario);
+                        MessageBox.Show("Se registro el usuario correctamente");
+                    };
+                    break;
+                case "Gerente":
+                    unUsuario = new BE.Administrador();
+                    {
+                        unUsuario.Nombre = tboxNombreUsuarioCrear.Text;
+                        unUsuario.Apellido = tboxApellidoUsuarioCrear.Text;
+                        unUsuario.NombreUsuario = tboxNUsuarioCrear.Text;
+                        unUsuario.Contraseña = tboxContraseñaUsuarioCrear.Text;
+                        unUsuario.DNI = int.Parse(tboxDniUsuario.Text);
+                        //unUsuario.Rol = comboxSeleccionRolNuevoUsuario.SelectedItem.ToString();
+                        unUsuario.Rol = "2";
+                        unAdmin.RegistrarUsuario(unUsuario);
+                        MessageBox.Show("Se registro el usuario correctamente");
+                    };
+                    break;
+                case "Vendedor":
+                    unUsuario = new BE.Administrador();
+                    {
+                        unUsuario.Nombre = tboxNombreUsuarioCrear.Text;
+                        unUsuario.Apellido = tboxApellidoUsuarioCrear.Text;
+                        unUsuario.NombreUsuario = tboxNUsuarioCrear.Text;
+                        unUsuario.Contraseña = tboxContraseñaUsuarioCrear.Text;
+                        unUsuario.DNI = int.Parse(tboxDniUsuario.Text);
+                        //unUsuario.Rol = comboxSeleccionRolNuevoUsuario.SelectedItem.ToString();
+                        unUsuario.Rol = "3";
+                        unAdmin.RegistrarUsuario(unUsuario);
+                        MessageBox.Show("Se registro el usuario correctamente");
+                    };
+                    break;
+                case "Veterinario":
+                    unUsuario = new BE.Administrador();
+                    {
+                        unUsuario.Nombre = tboxNombreUsuarioCrear.Text;
+                        unUsuario.Apellido = tboxApellidoUsuarioCrear.Text;
+                        unUsuario.NombreUsuario = tboxNUsuarioCrear.Text;
+                        unUsuario.Contraseña = tboxContraseñaUsuarioCrear.Text;
+                        unUsuario.DNI = int.Parse(tboxDniUsuario.Text);
+                        //unUsuario.Rol = comboxSeleccionRolNuevoUsuario.SelectedItem.ToString();
+                        unUsuario.Rol = "4";
+                        unAdmin.RegistrarUsuario(unUsuario);
+                        MessageBox.Show("Se registro el usuario correctamente");
+                    };
+                    break;
+            }
+
         }
     }
 }
